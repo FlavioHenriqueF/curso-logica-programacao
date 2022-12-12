74f74c8634aa2f04318c05c8f89acca00b3309b0
Flavio Henrique <flavio3224henrique@gmail.com>
2022-12-12 20:29:41 -0300

    Java : Exercicio retangulo

---
+++ java/exercicio_retangulo/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Base do retangulo: ");
+		double base = sc.nextDouble();
+		System.out.print("Altura do retangulo: ");
+		double altura = sc.nextDouble();
+		
+		double area = base * altura;
+		double perimetro = (base + altura) * 2;
+		double diagonal = Math.sqrt(Math.pow(base,2) + Math.pow(altura, 2));
+		
+		System.out.println("AREA = " + String.format("%.4f", area));
+		System.out.println("PERIMETRO = " + String.format("%.4f", perimetro));
+		System.out.println("DIAGONAL = " + String.format("%.4f", diagonal));
+		
+	}
+
+}
